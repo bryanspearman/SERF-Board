b98e9425937e73263a10be02ca03a67ad2b3c6e9
Bryan Spearman <bryanspearman@yahoo.com>
2018-08-16 15:24:35 -0400

    testing out dummy data for layout purposes

--- public/index.js
+++ public/index.js
@@ -1,6 +1,23 @@
-$(function() {
-  console.log("jQuery running, adding some text to DOM");
+$(function placeHolderContent() {
+  $(".dynamic-1").append(
+    `<ul>
+      <li>
+        <p class="response-list-titles">Response Title 1</p>
+        <p class="response-list-blurps">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p>
+      </li>
+      <li>
+        <p class="response-list-titles">Response Title 2</p>
+        <p class="response-list-blurps">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p>
+      </li>
+      <li>
+        <p class="response-list-titles">Response Title 3</p>
+        <p class="response-list-blurps">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p>
+      </li>
+    </ul>`
+  );
   $(".dynamic-2").append(
     "<p><em>jQuery here. Just adding some text to the DOM, don't mind me</em></p>"
   );
 });
+
+$(placeHolderContent);
