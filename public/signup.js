b1b26e233643830ba76abf2a30f5bf145ad49416
Bryan Spearman <bryanspearman@yahoo.com>
2018-08-21 15:02:01 -0400

    added code to signup-js

--- public/signup.js
+++ public/signup.js
@@ -3,13 +3,11 @@
 function checkPass() {
   var pass1 = document.getElementById("pass1");
   var pass2 = document.getElementById("pass2");
-
   var message = document.getElementById("confirmMessage");
-
   var goodColor = "#66cc66";
   var badColor = "#ff6666";
 
-  if (pass1.value == pass2.value) {
+  if (pass1.value === pass2.value) {
     pass2.style.backgroundColor = goodColor;
     message.style.color = goodColor;
     message.innerHTML = "Passwords Match!";
@@ -19,3 +17,49 @@ function checkPass() {
     message.innerHTML = "Passwords Do Not Match!";
   }
 }
+
+const signUpURL = "https://gentle-lake-36024.herokuapp.com/api/users";
+
+function postAccountInfo(userData, callback) {
+  const settings = {
+    url: signUpURL,
+    data: {
+      userName: userData.userName,
+      password: userData.password,
+      firstName: userData.firstName,
+      lastName: userData.lastName
+    },
+    dataType: "json",
+    type: "POST",
+    success: callback
+  };
+  $.ajax(settings);
+}
+
+function signUpCallback() {
+  window.location = "/login.html";
+}
+
+function createAccount() {
+  $(".js-signUp-form").submit(event => {
+    event.preventDefault();
+    const queryFirstName = document.getElementById("firstName").value;
+    const queryLastName = document.getElementById("lastName").value;
+    const queryUserName = document.getElementById("userName").value;
+    const queryPassword = document.getElementById("pass2").value;
+    const userData = {
+      firstName: queryfirstName,
+      lastName: querylastName,
+      userName: queryUserName,
+      password: queryPassword
+    };
+    queryFirstName.val("");
+    queryLastName.val("");
+    queryUserName.val("");
+    queryPassword.val("");
+    queryFirstName.focus();
+    postAccountInfo(userData, signUpCallback);
+  });
+}
+
+$(createAccount);
